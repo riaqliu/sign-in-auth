afb860b6f40eb00a480b637bbf8c82052d22f7ae
riaqliu <riaqliu@gmail.com>
2025-02-17 11:47:41 +0800

    Add Frontend Signin Template

---
+++ my-app-vue/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import SignInComponent from './components/SignInComponent.vue';
+</script>
+
+<template>
+  <div class="sign-in-form">
+    <SignInComponent/>
+  </div>
+</template>
+
+<style scoped>
+  .sign-in-form {
+    display: inline-block;
+    text-align: left;
+  }
+</style>
---
+++ my-app-vue/src/components/SignInComponent.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+    name: "SignInComponent",
+    data() {
+        return {
+            username: '',
+            password: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            this.clearForm();
+        },
+
+        clearForm() {
+            this.username = '';
+            this.password = '';
+        }
+    }
+}
+</script>
+<template>
+    <div class="form-body">
+        <h1>Sign In</h1>
+        <form @submit.prevent="onSubmit()">
+            <p>Username: <input type="text" v-model="username" required></p>
+            <p>Password: <input type="password" v-model="password" required></p>
+            <button type="submit">Sign In</button>
+        </form>
+    </div>
+</template>
+<style scoped>
+    h1 {
+        text-align: center;
+    }
+    button {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width:100%;
+        height: 30px;
+        border-radius: 10px;
+    }
+    .form-body {
+        background-color: white;
+        padding: 20px 40px 40px 40px;
+        margin: 5px;
+        border-radius: 10px;
+        border-color: black;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+</style>
